722d8913b26d19022b494b01b8ac51d19a300352
JaumeJulia <62952723+JaumeJulia@users.noreply.github.com>
2023-04-07 10:57:29 +0200

    Removal of non-used components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { BodyComponent } from './shared/body/body.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BackgroundComponent } from './components/background/background.component';
-import { StyledButtonComponent } from './components/styled-button/styled-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     BodyComponent,
     BackgroundComponent,
-    StyledButtonComponent
   ],
   imports: [
     BrowserModule,
